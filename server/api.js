0e007e07aed3c07a21693daa3fdb199ad7c2d425
Kurikas <Suvihj@gmail.com>
2025-06-08 19:01:27 +0300

    initial db added

--- server/api.js
+++ server/api.js
@@ -1,18 +1,36 @@
 import express from "express";
+import { Sequelize, DataTypes } from 'sequelize';
 
-  export const api = new express.Router();
+export const api = new express.Router();
   
-  const EXAMPLES = [
-      { example: "test" },
-      { example: "dummy" },
-  ];
-  
-  api.get("/api/examples", (req, res) => {
-      res.send(EXAMPLES)
+// Use SQLite — the DB file will be 'database.sqlite' in your repo
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'database.sqlite',
+});
+
+// Example model
+const User = sequelize.define('User', {
+  username: DataTypes.STRING,
+  birthday: DataTypes.DATE,
+});
+
+  api.get("/api/examples", async (req, res) => {
+   const users = await User.findAll();
+    res.send(users.map(user => user.toJSON()));
+    //res.send(User.toJSON());
   });
   
   api.post("/api/examples", (req, res) => {
       const { example } = req.body;
-      EXAMPLES.push({ example, id: EXAMPLES.length });
+      // Sync and use the model
+        (async () => {
+          await sequelize.sync();
+          const jane = await User.create({
+            username: 'janedoe',
+            birthday: new Date(1990, 6, 20),
+          });
+        })();
+        console.log(jane.toJSON());
       res.sendStatus(204);
   });
